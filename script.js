8079b0405e70689f1ac1d16e4e0d207f2fc610d3
Glitch (hello-webpage) <none>
2019-12-22 20:54:38 +0000

    🐠🚄 Checkpoint ./.glitch-assets:9625737/376 ./script.js:9625737/344

--- script.js
+++ script.js
@@ -70,6 +70,19 @@ function drawText(text, sx, sy) {
   }
 }
 
+var audioContext;
+window.addEventListener('load', init, false);
+function init() {
+  try {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    audioContext = new AudioContext();
+  }
+  catch(e) {
+    alert('Web Audio API is not supported in this browser');
+  }
+}
+
 let state = {};
 
 function resetState() {
